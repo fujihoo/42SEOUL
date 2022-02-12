22caa6b96338ea968379eb7341364b50a3b95f66
Jihoo Park <jihoopar@c8r4s12.42seoul.kr>
2022-02-12 21:20:36 +0900

    C00_ex07

--- 1.la_piscine/4.C00/ex07/ft_putnbr.c
+++ 1.la_piscine/4.C00/ex07/ft_putnbr.c
@@ -1,3 +1,23 @@
 #include <unistd.h>
 
-void ft_
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb >= -2147483648 && nb <= 2147483647)
+	{
+		while (nb / 10 + 48
+
+		ft_putchar(nb / 10 + 48);
+		ft_putchar(nb % 10 + 48);
+	}
+	else
+	{
+		return ;
+	}	
+}
+
+int	main(void)
