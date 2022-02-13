2d9665f4d0648bd035a062ca06f61e552f60a888
fujihoo <jifupark@naver.com>
2022-02-13 21:06:34 +0900

    incomplete

--- 1.la_piscine/4.C00/ex07/ft_putnbr.c
+++ 1.la_piscine/4.C00/ex07/ft_putnbr.c
@@ -1,3 +1,45 @@
 #include <unistd.h>
 
-void ft_
+void	ft_putchar(int c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_cal(int nb)
+{
+	if (nb >= 10)
+	{
+		ft_putchar(nb / 10 + 48);
+		return ft_cal(nb);
+	}
+	else
+	{
+		ft_putchar(nb % 10 + 48);
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+	}
+	else if (nb > -2147483648 && nb < 0)
+	{
+		nb = nb * -1;
+		write (1, "-", 1);
+		ft_cal(-nb);
+	}
+	else if (nb < 2147483647 && nb >= 0)
+	{
+		ft_cal(nb);
+	}
+}
+
+int	main(void)
+{
+	ft_putnbr(42);
+
+	return (0);
+}
+
